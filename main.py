6cb4821e8cd0c1fc5bb282cb0da6836a433a734f
Eric Dou <zhongfan.dou@zawee.work>
2020-07-22 17:09:49 +0800

    Fix some checking calculation

--- main.py
+++ main.py
@@ -8,9 +8,11 @@ if __name__ == '__main__':
 
     IO = 'Solar PV Model Rev1.6.xlsx'
     df = pd.read_excel(io=IO, sheet_name='Data Dump', usecols="A:C", parse_dates=['TimeStamp'])
-    df_org = pd.read_excel(IO, sheet_name='Model Calcs', usecols="AB:AD").dropna()
+    df_org = pd.read_excel(IO, sheet_name='Model Calcs', usecols="A:Z", parse_dates=['Date']).dropna()
     df = calculate_angle(df)
-
+    result = sum( list(a - b for a,b in zip(df[col_solar_azimuth_ang], df_org[col_solar_azimuth_ang])))
+    print(result)
     df = calculate_radiation(df)
-    result = (df[col_dhi] - df[col_dhi]).sum()
+    df_org = pd.read_excel(IO, sheet_name='Model Calcs', usecols="AB:AD").dropna()
+    result = sum( list(a - b for a,b in zip(df[col_dhi], df_org[col_dhi])))
     print(result)
