6d964f381009b24d6f74300783649dc5ff6d5602
Yogipip <68209817+Yogipip@users.noreply.github.com>
2020-07-22 12:54:38 +1000

    Update AOI.py

--- AOI.py
+++ AOI.py
@@ -1,1 +1,8 @@
-def 
+import math
+def AOI(orient:float,tilt:float,zenith:float,azimuth:float):
+    AOI_rad=math.sin(math.radians(zenith))*math.cos(math.radians(azimuth-orient))*math.sin(math.radians(tilt))\
+    +math.cos(math.radians(z))*math.cos(math.radians(tilt))
+    AOI_deg=math.degrees(math.acos(AOI_rad))
+return AOI_rad,AOI_deg 
+AOI()
+  
