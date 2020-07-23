f724a102a83bd28a20c32bf7d8a704e56f32d614
Yogipip <68209817+Yogipip@users.noreply.github.com>
2020-07-23 13:08:19 +1000

    Update perez_model.py

--- src/perez_model.py
+++ src/perez_model.py
@@ -2,7 +2,9 @@ import math
 import pandas as pd
 
 from util import col_perez_a, col_perez_b, col_perez_sky, col_aoi_deg, col_solar_zen_ang, \
-    col_perez_sky, col_dni, col_ghi, col_dhi, col_perez_air_mass_optical, col_perez_delta
+    col_perez_sky, col_dni, col_ghi, col_dhi, col_perez_air_mass_optical, col_perez_delta, \
+    col_perez_f11, col_perez_f12, col_perez_f13, col_perez_f21, col_perez_f22, col_perez_f23, \
+    col_perez_F1, col_perez_F2, col_model, col_Di, col_Dc, col_Dh
 
 # Look up table for f11,f12,f13,f21.f22 and f23 calculations
 Look_Up = [[-0.008312, 0.587728, -0.06206, -0.59601, 0.072125, -0.022022],
@@ -61,37 +63,57 @@ def get_perez_air_mass(zenith: float, perez_b: float):
 def get_perez_delta(dhi: float, perez_air_mass_optical: float):
     return (dhi * perez_air_mass_optical) / 1367
 
+def get_perez_param(perez_sky:float,perez_delta:float,zenith:float):
+     
+     i = int(perez_sky > 1.065) + int(perez_sky > 1.23) + int(perez_sky > 1.5) + int(perez_sky > 1.95) \
+            + int(perez_sky > 2.8) + int(perez_sky > 4.5) + int(6.2 < perez_sky <= 100000)
+    
+     perez_f11, perez_f12, perez_f13, perez_f21, perez_f22, perez_f23 = [Look_Up[i][j] for j in range(6)]
+     
+     perez_F1 = max(0.0, perez_f11 + (perez_delta * perez_f12) + (math.radians(zenith) * perez_f13))
+    
+     perez_F2 = perez_f21 + (perez_delta * perez_f22) + (math.radians(zenith) * perez_f23)
+     
+     return perez_f11, perez_f12, perez_f13, perez_f21, perez_f22, perez_f23,perez_F1,perez_F2
+
+def get_diffuse_values(dhi:float, tiltangle:float,zenith:float,perez_F1 :float,perez_F2 :float):
+    
+    if 87.5 <= zenith <= 90.0:
+        model = 'Isotropic'
+        Di = 0.5 * dhi * (1 + math.cos(math.radians(tiltangle)))
+        Dc = 0.0
+        Dh = 0.0
+    
+    else:
+        model = 'Perez'
+        Di = 0.5 * dhi * (1 - perez_F1) * (1 + math.cos(math.radians(tiltangle)))
+        Dc = dhi * perez_F1 * perez_a / perez_b
+        Dh = dhi * perez_F2 * math.sin(math.radians(tiltangle))
+        
+        return model, Di, Dc, Dh
+
 
 def perez(df: pd.DataFrame, tiltangle: float):
+    
     df[col_perez_a] = df[col_aoi_deg].map(get_perez_a)
+    
     df[col_perez_b] = df[col_solar_zen_ang].map(get_perez_b)
+    
     df[col_perez_sky] = df.apply(lambda x: (x[col_dni] + x[col_dhi]) / (
             (x[col_dhi] + 5.534e-6) * x[col_solar_zen_ang] ** 3 * (
-            1 + (5.534e-6 * x[col_solar_zen_ang] ** 3))), axis=1)
+            1 + (5.534e-6 * x[col_solar_zen_ang] ** 3))))
+    
     df[col_perez_air_mass_optical] = df.apply(
-        lambda x: get_perez_air_mass(x[col_solar_zen_ang], x[col_perez_b]), axis=1)
+        lambda x: get_perez_air_mass(x[col_solar_zen_ang], x[col_perez_b]))
+    
     df[col_perez_delta] = df.apply(
-        lambda x: get_perez_delta(x[col_dhi], x[col_perez_air_mass_optical]), axis=1)
-
-    # i = int(perez_sky > 1.065) + int(perez_sky > 1.23) + int(perez_sky > 1.5) + int(
-    #     perez_sky > 1.95) \
-    #     + int(perez_sky > 2.8) + int(perez_sky > 4.5) + int(6.2 < perez_sky <= 100000)
-    #
-    # f11, f12, f13, f21, f22, f23 = [Look_Up[i][j] for j in range(6)]
-    #
-    # perez_F1 = max(0, f11 + (perez_delta * f12) + (math.radians(zenith) * f13))
-    #
-    # perez_F2 = f21 + (perez_delta * f22) + (math.radians(zenith) * f23)
-    #
-    # if 87.5 <= zenith <= 90:
-    #     model = 'Isotropic'
-    #     Di = 0.5 * dhi * (1 + math.cos(math.radians(tilt_ang)))
-    #     Dc = 0
-    #     Dh = 0
-    # else:
-    #     model = 'Perez'
-    #     Di = 0.5 * dhi * (1 - perez_F1) * (1 + math.cos(math.radians(tiltangle)))
-    #     Dc = dhi * perez_F1 * perez_a / perez_b
-    #     Dh = dhi * perez_F2 * math.sin(math.radians(tiltangle))
-
+        lambda x: get_perez_delta(x[col_dhi], x[col_perez_air_mass_optical]))
+    
+    df[col_perez_f11],df[col_perez_f12],df[col_perez_f13],df[col_perez_f21],
+    df[col_perez_f22], df[col_perez_f23],df[col_perez_F1],df[col_perez_F2] = df.apply(
+        lambda x: get_perez_param(x[col_perez_sky],x[col_perez_delta],x[col_solar_zen_ang])
+     
+    df[col_model],df[col_Di], df[col_Dc], df[col_Dh]= df.apply(
+        lambda x: get_diffuse_values(x[col_dhi],tiltangle,x[col_solar_zen_angle],x[col_perez_F1],x[col_perez_F2]))
+     
     return df
